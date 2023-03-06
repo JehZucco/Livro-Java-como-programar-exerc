a87d4408b14bfa04b8c9d7eff66bb82246d528c5
JehZucco <andressa0360@gmail.com>
2023-03-06 10:42:24 -0300

    cap2Quest2ponto31

---
+++ QuadradoECuboNumerosZeroADez.java
@@ -0,0 +1,28 @@
+/** Tabela de quadrados e cubos: utilizando apenas as técnicas de programação apresentadas no cap 2,
+ * escreva um aplicativo que calcule os quadrados e cubosdos numeros de 0 a 10
+ * e imprima os valores resultantes no formato de tabela.
+ * */
+
+public class QuadradoECuboNumerosZeroADez {
+
+    public static void main(String[] args) {
+        /** A questão não especifíca, porém optei por não guardar os valores em nenhuma variável
+         *E como é com as técnicas de programação no cap 2, não tem muitas opções de soluções
+         * */
+
+        System.out.println("Número     Quadrado     Cubo");
+        System.out.printf("%d           %d           %d%n", 0,0,0);
+        System.out.printf("%d           %d           %d%n", 1,1*1,1*1*1);
+        System.out.printf("%d           %d           %d%n", 2,2*2,2*2*2);
+        System.out.printf("%d           %d           %d%n", 3,3*3,3*3*3);
+        System.out.printf("%d           %d          %d%n", 4,4*4,4*4*4);
+        System.out.printf("%d           %d          %d%n", 5,5*5,5*5*5);
+        System.out.printf("%d           %d          %d%n", 6,6*6,6*6*6);
+        System.out.printf("%d           %d          %d%n", 7,7*7,7*7*7);
+        System.out.printf("%d           %d          %d%n", 8,8*8,8*8*8);
+        System.out.printf("%d           %d          %d%n", 9,9*9,9*9*9);
+        System.out.printf("%d          %d         %d%n", 10,10*10,10*10*10);
+
+    }
+}
+
